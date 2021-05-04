ee793f90a77d0f017e7a34274b48f41f425cadc2
Filipe Andrade <filipe.engenharia42@gmail.com>
2021-05-04 20:54:54 -0300

    added go.mod file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/filipeandrade6/aprenda-go-com-testes
+
+go 1.15
--- ola_test.go
+++ ola_test.go
@@ -3,9 +3,25 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := Ola("Chris")
-	esperado := "Olá, Chris"
-	if resultado != esperado {
-		t.Errorf("resultado'%s', esperado '%s'", resultado, esperado)
+	verificarMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		// Necessário para dizermos ao conjunto de testes que este é um método auxiliar
+		// Ao fazer isso, quando o teste falhar, o número da linha relatada estará em
+		// nossa chamada de função, e não dentro do nosso auxiliar de teste.
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
 	}
+
+	t.Run("diz olá para as pessoas", func(t *testing.T) {
+		resultado := Ola("Chris")
+		esperado := "Olá, Chris"
+		verificarMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("diz 'Olá, mundo', quando uma string vazia for passada", func(t *testing.T) {
+		resultado := Ola("")
+		esperado := "Olá, mundo"
+		verificarMensagemCorreta(t, resultado, esperado)
+	})
 }
