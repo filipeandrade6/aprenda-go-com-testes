14969c1e558decc6d873ce2b2e2ea91fb2f9b419
Filipe Andrade <filipe.engenharia42@gmail.com>
2021-05-15 17:42:13 -0300

    added contador

---
+++ contador/contador.go
@@ -0,0 +1,26 @@
+package contador
+
+import "sync"
+
+// Contador incrementa um número
+type Contador struct {
+	mu    sync.Mutex
+	valor int
+}
+
+// NovoContador retorna um novo Contador
+func NovoContador() *Contador {
+	return &Contador{}
+}
+
+// Incrementa o contador
+func (c *Contador) Incrementa() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.valor++
+}
+
+// Valor retorna a contagem atual
+func (c *Contador) Valor() int {
+	return c.valor
+}
